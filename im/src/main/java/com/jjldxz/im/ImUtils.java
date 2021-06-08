cee17690893b499f033d873f08d359df0226ddf6
Zazazach <zach_huo@163.com>
2021-06-08 17:12:31 +0800

    上传3

--- im/src/main/java/com/jjldxz/im/ImUtils.java
+++ im/src/main/java/com/jjldxz/im/ImUtils.java
@@ -1,8 +1,13 @@
 package com.jjldxz.im;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
 public class ImUtils {
 
-    public String setText(String stirng){
-        return "IM_module"+stirng;
+    public static String getTime(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));
+        return  (""+calendar.get(Calendar.YEAR))+(""+(calendar.get(Calendar.MONTH) + 1))+(""+calendar.get(Calendar.DAY_OF_MONTH));
     }
 }
